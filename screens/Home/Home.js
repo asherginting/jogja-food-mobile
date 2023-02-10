845835e9cfcc853fcaa90cd6270ae8e33a4c819c
Asher Azriel Ginting <asherginting3@gmail.com>
2023-02-11 00:48:20 +0700

    feat: init all setup

---
+++ screens/Home/Home.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const Home = () => {
+  return (
+    <View
+      // eslint-disable-next-line react-native/no-inline-styles
+      style={{
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+      }}>
+      <Text>Home</Text>
+    </View>
+  );
+};
+
+export default Home;
